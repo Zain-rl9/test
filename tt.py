8277915940db275f455929fa3bcf397c61652d3f
zain-rl9 <mdzainrl9@gmail.com>
2023-08-30 18:36:07 +0530

    mern st

--- tt.py
+++ tt.py
@@ -1,7 +1,7 @@
 def func():
  try:
     l=[4,5,8,3,4]
-    i=int(input("enter:"))
+    i=int(input("enter the number for search:"))
     print(l[i])
     return 1
  except:
@@ -9,5 +9,6 @@ def func():
     return 0
  finally:
     print("cyber devil")
+    print("foot cybe")
 x=func()
 print(x)
